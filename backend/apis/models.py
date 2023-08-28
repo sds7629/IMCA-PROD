18bcb7dd1fcd7714f7481877306ba24aa62a3c52
jinwoo <sds7629>
2023-08-28 17:58:08 +0900

    name, unique

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class FestivalModel(models.Model):
-    api_id = models.CharField(max_length=50)
+    api_id = models.CharField(max_length=50, unique=True)
     start_date = models.DateField(null=False, blank=False)
     end_date = models.DateField(null=False, blank=False)
     poster = models.URLField(null=True, blank=True)
     place = models.CharField(max_length=15)
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=60)
